f11dac3a6e6201c1c69cdfb649c706acff30c723
Max <max@schrodingcat.com>
2022-12-16 01:56:04 +0800

    remove redundent code

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
-  // NOTE: test
-  // let service: ConfigService = new ConfigService()
-  // let g_c = new GestureConfig([new GestureSetting(GestureType.backward, EffectType.local, [
-  //     new GestureEffect(new StateCommand(0, "test_command", FunctionType.light, (s)=>{return s}), new Agent(0, "test", [], AgentLocation.attic))
-  //   ], new Agent(0, "test", [], AgentLocation.attic))
-  // ]);
-  // service.saveGestureConfig(g_c)
-  // let g: GestureConfig = service.readGestureConfig()
-  // console.log(g)
-  // console.log(g.gesture_settings[0].effects[0].command.state_function)
-
   let service: ConfigService = new ConfigService();
   service.saveAgentConfig({ agents: [] });
   service.saveGestureConfig({ gestureSettings: [] });
